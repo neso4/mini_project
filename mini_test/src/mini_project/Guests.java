b0114cb0006df1f5734419d43513929f2655cfd7
7suy7 <67411126+7suy7@users.noreply.github.com>
2020-07-06 17:12:32 +0900

    게스트 클래스 파일

--- mini_test/src/mini_project/Guests.java
+++ mini_test/src/mini_project/Guests.java
@@ -1,5 +1,58 @@
 package mini_project;
 
 public class Guests {
+private String custname;
+private String email;
+private String phonenumber;
+private String usedate;
+private int counts;
+
+public Guests() {
+}
+
+public Guests(String custname,String email,String phonenumber, String usedate) {
+	super();
+	this.custname=custname;
+	this.email=email;
+	this.phonenumber=phonenumber;
+	this.usedate = usedate;
 	
+}
+
+public String getCustname() {
+	return custname;
+}
+
+public void setCustname(String custname) {
+	this.custname = custname;
+}
+
+public String getEmail() {
+	return email;
+}
+
+public void setEmail(String email) {
+	this.email = email;
+}
+
+public String getPhonenumber() {
+	return phonenumber;
+}
+
+public void setPhonenumber(String phonenumber) {
+	this.phonenumber = phonenumber;
+}
+
+public String getUsedate() {
+	return usedate;
+}
+
+public void setUsedate(String usedate) {
+	this.usedate = usedate;
+}
+
+
+
+
+
 }
